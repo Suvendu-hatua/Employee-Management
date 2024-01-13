e9b7e1af473c8eb484474d1eda9d973f89a2654c
Suvendu-hatua <97820942+Suvendu-hatua@users.noreply.github.com>
2024-01-13 17:19:09 +0530

    Add files via upload

--- SpringBoot/src/main/java/com/myrepository/EmployeeRepository.java
+++ SpringBoot/src/main/java/com/myrepository/EmployeeRepository.java
@@ -1,1 +1,11 @@
+package com.myrepository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springbootbeanfiles.EmployeeBean;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeBean, Long> {
+
+}
